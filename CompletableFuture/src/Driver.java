145f898f3970b898f34245081fc6528217f0d5e7
spb.arun.01@gmail.com <arun.dhwaj@smartbitpixel.co.in>
2019-11-08 22:08:35 +0530

    Rule1, Rule2, Rule3 moved to MyCompletable.java

--- CompletableFuture/src/Driver.java
+++ CompletableFuture/src/Driver.java
@@ -6,56 +6,11 @@ public class Driver
     public static void main(String[] args) throws ExecutionException, InterruptedException
     {
         System.out.println("CompletableFuture brief tutorial");
-        //completedFuture1();
-        //completedFuture2();
-        completedFuture3();
+
+        MyCompletableFuture myCompletableFuture = new MyCompletableFuture();
+
+        //myCompletableFuture.completedFuture1();
+        //myCompletableFuture.completedFuture2();
+        myCompletableFuture.completedFuture3();
     }
-
-    private static void completedFuture1() throws ExecutionException, InterruptedException
-    {
-        //1: A new CompletableFuture is returned that is already completed with the given value.
-
-        String str = "1) A new CompletableFuture is returned that is already completed with the given value.";
-        CompletableFuture<String> cf = CompletableFuture.completedFuture(str);
-
-        if (cf.isDone())
-        {
-            System.out.println("Value:: " + cf.get());
-        }
-    }
-
-    private static void completedFuture2() throws ExecutionException, InterruptedException
-    {
-        //2: Running an asynchronous task using runAsync(Runnable runnable) method.
-        // This method returns a CompletableFuture<Void>.
-
-        String str = "2: Running an asynchronous task using runAsync(Runnable runnable) method. This method returns a CompletableFuture<Void>.";
-
-
-        CompletableFuture<Void> cf = CompletableFuture.runAsync(()->
-        {
-            System.out.println("Task executing asynchronously");
-            System.out.println(str);
-        });
-
-        System.out.println("Value- " + cf.get());
-    }
-
-    private static void completedFuture3() throws ExecutionException, InterruptedException
-    {
-        //3: runAsync() is fine for running asynchronous computations but it doesn't return value.
-        // If you want to return a new CompletableFuture with a value then you can use supplyAsync(Supplier<U> supplier) method.
-        // Here U is the type of value obtained by calling the given Supplier.
-
-        String str = "3.1: runAsync() is fine for running asynchronous computations but it doesn't return value. If you want to return a new CompletableFuture with a value then you can use supplyAsync(Supplier<U> supplier) method. Here U is the type of value obtained by calling the given Supplier.";
-
-        CompletableFuture cf = CompletableFuture.supplyAsync(()->
-        {
-            System.out.println(str);
-            return str;
-        });
-
-        System.out.println("3.2:: " + cf.get());
-    }
-
 }
